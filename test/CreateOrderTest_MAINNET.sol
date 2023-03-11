a3f442e692b21fd820f2ab20a58040a8a6e5beb9
Alfred Gaillard <alfred.gaillard@smart-chain.fr>
2023-03-11 18:36:32 +0100

    Seems that I can't transfer tokens

--- test/CreateOrderTest_MAINNET.sol
+++ test/CreateOrderTest_MAINNET.sol
@@ -11,7 +11,6 @@ import {IRewardsController} from "@yield-daddy/src/aave-v3/external/IRewardsCont
 import {ISwapRouter} from "@v3-periphery/interfaces/ISwapRouter.sol";
 import {ERC20} from "@solmate/tokens/ERC20.sol";
 import {ERC4626} from "@solmate/mixins/ERC4626.sol";
-import {WETH9} from "./external/WETH9.sol";
 
 import {OrderBook} from "../src/Orderbook.sol";
 import {OrderExecutor} from "../src/OrderExecutor.sol";
@@ -21,83 +20,73 @@ import {LendingVault} from "../src/LendingVault.sol";
 contract CreateOrderTest is Test {
 
     // Main contracts
-    OrderBook orderbook;
+    OrderBook orderBook;
     OrderExecutor orderExecutor;
     LendingVault lendingVault;
 
     // External contracts
     ERC4626 underleyingAssetVault;
 
-    function test_Deploy() public {
+    // Data
+    uint256 orderNonce;
+
+    function setUp() public {
+        deploy();
+    }
+
+    function deploy() internal {
         // Deploy OrderBook
-        orderbook = new OrderBook(vm.envAddress("OPS_MAINNET"));
+        orderBook = new OrderBook(vm.envAddress("OPS_MAINNET"));
 
         // Deploy OrderExecutor
-        orderExecutor = new OrderExecutor(vm.envAddress("OPS_MAINNET"), address(orderbook), vm.envAddress("SwapRouter_MAINNET"));
+        orderExecutor = new OrderExecutor(vm.envAddress("OPS_MAINNET"), address(orderBook), vm.envAddress("SwapRouter_MAINNET"));
 
         // Set OrderBook's executor
-        orderbook.setOrderExecutor(orderExecutor);
+        orderBook.setOrderExecutor(orderExecutor);
 
         // Deploy LendingVault
-        lendingVault = new LendingVault(address(orderbook));
+        lendingVault = new LendingVault(address(orderBook));
 
         // Set OrderBook's lendingVault
-        orderbook.setLendingVault(address(lendingVault));
+        orderBook.setLendingVault(address(lendingVault));
 
         // Set OrderExecutor's lendingVault
         orderExecutor.setLendingVault(address(lendingVault));
     }
 
-    function test_ERC4626CreationForCompatibleAsset() public {
+    function ERC4626CreationForCompatibleAsset(address tokenIn) internal {
         IPoolAddressesProvider aavePoolAddressesProvider = IPoolAddressesProvider(vm.envAddress("IPoolAddressesProvider_MAINNET"));
         IPool aavePool = IPool(aavePoolAddressesProvider.getPool());
         AaveV3ERC4626Factory aaveFactory = new AaveV3ERC4626Factory(aavePool, address(0x0), IRewardsController(address(0x0)));
 
         // Create ERC4626 for USDC
-        ERC20 underleyingAsset = ERC20(vm.envAddress("USDC_MAINNET"));
+        ERC20 underleyingAsset = ERC20(tokenIn);
         underleyingAssetVault = aaveFactory.createERC4626(underleyingAsset);
 
-        assertEq(address(underleyingAssetVault.asset()), address(underleyingAsset));
+        assertEq(address(underleyingAssetVault.asset()), address(underleyingAsset), "ERC4626 is not created for tokenIn asset");
     }
 
-    function test_getUSDC() public {
-        // Define the USDC token
-        ERC20 underleyingAsset = ERC20(vm.envAddress("USDC_MAINNET"));
-        uint256 underleyingAssetAmountBeforeSwap = underleyingAsset.balanceOf(msg.sender);
+    function test_CreateUSDCToWETHOrder() public {
 
-        // Give 100 ethers to the address that is gonna pass an order
-        hoax(msg.sender, 10000000000000000000);
-        // Wrap 100 ethers into WETH
-        WETH9 weth = WETH9(payable(vm.envAddress("WETH_MAINNET")));
-        // Give 100 ethers to the address that is gonna give WETH to the sender
-        weth.deposit{value : 10000000000000000000}();
-        uint256 wethBalance = weth.balanceOf(msg.sender);
-        assertEq(wethBalance, 10000000000000000000);
+        ERC4626CreationForCompatibleAsset(vm.envAddress("USDC_MAINNET"));
 
-        // Swap 100 WETH for USDC
-        ISwapRouter swapRouter = ISwapRouter(vm.envAddress("SwapRouter_MAINNET"));
-        // Approving the appropriate amount that uniswap is gonna take on order to make the swap
-        weth.approve(vm.envAddress("SwapRouter_MAINNET"), weth.balanceOf(msg.sender));
-        ERC20(vm.envAddress("WETH_MAINNET")).approve(vm.envAddress("SwapRouter_MAINNET"), weth.balanceOf(msg.sender));;
-        // Naively set amountOutMinimum to 0. In production, use an oracle or other data source to choose a safer value for amountOutMinimum.
-        // We also set the sqrtPriceLimitx96 to be 0 to ensure we swap our exact input amount.
-        ISwapRouter.ExactInputSingleParams memory params =
-            ISwapRouter.ExactInputSingleParams({
-                tokenIn: vm.envAddress("WETH_MAINNET"),
-                tokenOut: vm.envAddress("USDC_MAINNET"),
-                fee: 3000, // For this example, we will set the pool fee to 0.3%.
-                recipient: msg.sender,
-                deadline: block.timestamp,
-                amountIn: weth.balanceOf(msg.sender),
-                amountOutMinimum: 0, // NOT IN PRODUCTION
-                sqrtPriceLimitX96: 0 // NOT IN PRODUCTION
-            });
+        // Give 100000 usdc to the address that is gonna pass an order
+        deal(vm.envAddress("USDC_MAINNET"), msg.sender, 100000000000, true);
+        require(ERC20(vm.envAddress("USDC_MAINNET")).balanceOf(msg.sender) == 100000000000, "Not enough USDC");
 
-        // The call to `exactInputSingle` executes the swap.
-        swapRouter.exactInputSingle(params);
+        // Approve the orderbook to spend the 100000 usdc
+        ERC20(vm.envAddress("USDC_MAINNET")).approve(address(orderBook), 100000000000);
 
-        // Check that the swap was successful
-        require(underleyingAsset.balanceOf(msg.sender) > underleyingAssetAmountBeforeSwap, "Swap failed");
+        // Create an order
+        orderNonce = orderBook.createOrder(123, 1, underleyingAssetVault, vm.envAddress("WETH_MAINNET"));
+
+        // Check if the order is created
+        //assertEq(orderBook.getOrder(orderNonce).user, msg.sender, "Order is not created");
+    }
+
+    function testFail_OrderNotPassed() internal {
+        // Check if the order is passed
+        assertEq(orderBook.getOrder(orderNonce).isExecuted, true, "Order is not passed");
     }
 /*
     function testFail_Subtract43() public {
